471a2d3e6e656d5cef8f062657881b1ed77c81eb
alynn01 <alynn@essolo.com>
2023-03-20 15:03:15 +0100

    fixed failing test
    updated failing test

--- cypress/support/pages/cards/cardToCardTransfer-page.js
+++ cypress/support/pages/cards/cardToCardTransfer-page.js
@@ -5,11 +5,11 @@ export class CardToCardTransferPage{
   pageTitle = () => cy.get(`[class='cardTitle']`);
   cardDropdown = () => cy.get('app-dropdown-icon > #dropdownchild');
   clientIdentification = () => cy.get('.cardRow > :nth-child(2) > .formInputWrapper');
-  lastFourDigits = () => cy.get('.cardRow > :nth-child(3) > .formInputWrapper');
+  lastFourDigits = () => cy.get('.cardRow > :nth-child(4) > .formInputWrapper');
   accountDropdownItem = () =>
     cy.get(`[id='dropdownchild']`).contains(" USD - 6122256485 ");
   cardLastFourDropdownItem = () => cy.get(`[id='dropdownchild']`);
-  amountEntryField = () => cy.get('.cardRow > :nth-child(4) > .formInputWrapper');
+  amountEntryField = () => cy.get(':nth-child(1) > .cardRow > form-input-large > .formInputWrapper');
 
   transferAmountButton = () =>
     cy.get(`[class='formButton']`).contains(" Transfer ");
@@ -27,9 +27,9 @@ export class CardToCardTransferPage{
   enterTransferDetails(){
     this.cardDropdown().click({force : true})
     this.cardLastFourDropdownItem().contains("8852").click();
-    this.clientIdentification().type('34567890')
-    this.lastFourDigits().type('0032')
     this.amountEntryField().type('1')
+    this.clientIdentification().type('12706293')
+    this.lastFourDigits().type('0032')
     this.transferAmountButton().should('be.visible')
 
   }
