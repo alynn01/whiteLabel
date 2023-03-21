1536629e6407af3b6f736328969c89312df4cb3f
alynn01 <alynn@essolo.com>
2023-03-21 11:20:24 +0100

    added fund user and program
    updated test

---
+++ cypress/e2e/wallet/fundProgram.cy.js
@@ -0,0 +1,19 @@
+import { getProgramOwner } from "../../fixtures/index.js";
+import { CreateUserPage, FundProgramUserPage} from "../../support/pages/index";
+
+
+const newUserPage = new CreateUserPage();
+const fundProgram = new FundProgramUserPage()
+const programOwner = getProgramOwner();
+
+describe('Fund Program', () => {
+
+    beforeEach(() => {
+      newUserPage.accessPOPortal(programOwner.email, programOwner.password);
+    });
+
+    it('Test that user is able to navigate to fund program popup', () => {
+        fundProgram.accessFundProgramPage();
+        fundProgram.enterFundDetails();
+    })
+})
