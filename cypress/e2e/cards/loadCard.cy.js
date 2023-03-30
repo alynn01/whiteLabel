b327c332f3cb686e43f037be66d5965e9b7b51f3
alynn01 <alynn@essolo.com>
2023-03-30 09:22:55 +0100

    update load card feature
    load card feature updated

--- cypress/e2e/cards/loadCard.cy.js
+++ cypress/e2e/cards/loadCard.cy.js
@@ -13,21 +13,23 @@ describe('Load Card', () => {
     });
 
     it('Test that the user is able to access load physical card', () => {
-        loginPage.login(cardHolder.email, cardHolder.password);
+        loginPage.login("damiuser001@yopmail.com", "Password@1");
         cy.get('.onboarding-title').contains('Welcome');
         cy.get('.start').contains('Not interested').click();
-        cy.get('.title').contains('Amayindi').should('be.visible');
-        cardsPage.accessCardsPage();
+        cy.get('.title').contains('Damilare').should('be.visible');
+        cy.get('.app-onboarding-menu-cards > .nav-icon').click();
+        cy.wait(3000)
         loadCardPage.accessLoadCardPage();
         loadCardPage.loadPhysicalCard();
     })
 
     it('Test that the user is able to access load virtual card', () => {
-      loginPage.login(cardHolder.email, cardHolder.password);
+      loginPage.login("damiuser001@yopmail.com", "Password@1");
       cy.get('.onboarding-title').contains('Welcome');
       cy.get('.start').contains('Not interested').click();
-      cy.get('.title').contains('Amayindi').should('be.visible');
-      cardsPage.accessCardsPage();
+      cy.get('.title').contains('Damilare').should('be.visible');
+      cy.get('.app-onboarding-menu-cards > .nav-icon').click();
+      cy.wait(3000)
       loadCardPage.loadVirtualCard();
       
   })
