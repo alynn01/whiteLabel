3e425b3e0381045c2d1d6033960f7e3c33c63c04
alynn01 <127193049+alynn01@users.noreply.github.com>
2023-04-28 08:05:40 +0100

    Adde load card edge cases
    Added load card edge cases

--- cypress/e2e/cards/loadCard.cy.js
+++ cypress/e2e/cards/loadCard.cy.js
@@ -6,32 +6,72 @@ const cardsPage = new CardsPage();
 const loadCardPage = new LoadCardPage();
 const cardHolder = getCardHolder();
 
-describe('Load Card', () => {
+describe("Load Card", () => {
+  beforeEach(() => {
+    loginPage.accessLoginModal();
+  });
 
-    beforeEach(() => {
-      loginPage.accessLoginModal();
-    });
+  it("Test that the user is unable to load physical card with insufficient balance in account", () => {
+    loginPage.login("damiuser001@yopmail.com", "Password@1");
+    cy.get(".onboarding-title").contains("Welcome");
+    cy.get(".start").contains("Not interested").click();
+    cy.get(".title").contains("Damilare").should("be.visible");
+    cy.get(".app-onboarding-menu-cards > .nav-icon").click();
+    cy.wait(3000);
+    loadCardPage.accessLoadCardPage();
+    loadCardPage.loadPhysicalCard();
+  });
 
-    it('Test that the user is able to access load physical card', () => {
-        loginPage.login("damiuser001@yopmail.com", "Password@1");
-        cy.get('.onboarding-title').contains('Welcome');
-        cy.get('.start').contains('Not interested').click();
-        cy.get('.title').contains('Damilare').should('be.visible');
-        cy.get('.app-onboarding-menu-cards > .nav-icon').click();
-        cy.wait(3000)
-        loadCardPage.accessLoadCardPage();
-        loadCardPage.loadPhysicalCard();
-    })
+  it("Test that the user is unable to load physical card without amount", () => {
+    loginPage.login("damiuser001@yopmail.com", "Password@1");
+    cy.get(".onboarding-title").contains("Welcome");
+    cy.get(".start").contains("Not interested").click();
+    cy.get(".title").contains("Damilare").should("be.visible");
+    cy.get(".app-onboarding-menu-cards > .nav-icon").click();
+    cy.wait(3000);
+    loadCardPage.accessLoadCardPage();
+    loadCardPage.loadPhysicalCardWithoutAmount();
+  });
 
-    it('Test that the user is able to access load virtual card', () => {
-      loginPage.login("damiuser001@yopmail.com", "Password@1");
-      cy.get('.onboarding-title').contains('Welcome');
-      cy.get('.start').contains('Not interested').click();
-      cy.get('.title').contains('Damilare').should('be.visible');
-      cy.get('.app-onboarding-menu-cards > .nav-icon').click();
-      cy.wait(3000)
-      loadCardPage.loadVirtualCard();
-      
-  })
+  it("Test that the user is unable to load physical card without card details", () => {
+    loginPage.login("damiuser001@yopmail.com", "Password@1");
+    cy.get(".onboarding-title").contains("Welcome");
+    cy.get(".start").contains("Not interested").click();
+    cy.get(".title").contains("Damilare").should("be.visible");
+    cy.get(".app-onboarding-menu-cards > .nav-icon").click();
+    cy.wait(3000);
+    loadCardPage.accessLoadCardPage();
+    loadCardPage.loadPhysicalCardWithoutCardDetails();
+  });
 
-})
+  it("Test that the user is unable to load physical card without wallet details", () => {
+    loginPage.login("damiuser001@yopmail.com", "Password@1");
+    cy.get(".onboarding-title").contains("Welcome");
+    cy.get(".start").contains("Not interested").click();
+    cy.get(".title").contains("Damilare").should("be.visible");
+    cy.get(".app-onboarding-menu-cards > .nav-icon").click();
+    cy.wait(3000);
+    loadCardPage.accessLoadCardPage();
+    loadCardPage.loadPhysicalCardWithoutWalletDetails();
+  });
+
+  it("Test that the user is unable to load virtual card with insufficient balance in account", () => {
+    loginPage.login("damiuser001@yopmail.com", "Password@1");
+    cy.get(".onboarding-title").contains("Welcome");
+    cy.get(".start").contains("Not interested").click();
+    cy.get(".title").contains("Damilare").should("be.visible");
+    cy.get(".app-onboarding-menu-cards > .nav-icon").click();
+    cy.wait(3000);
+    loadCardPage.loadVirtualCard();
+  });
+
+  it("Test that the user is unable to load virtual card with insufficient balance in account", () => {
+    loginPage.login("damiuser001@yopmail.com", "Password@1");
+    cy.get(".onboarding-title").contains("Welcome");
+    cy.get(".start").contains("Not interested").click();
+    cy.get(".title").contains("Damilare").should("be.visible");
+    cy.get(".app-onboarding-menu-cards > .nav-icon").click();
+    cy.wait(3000);
+    loadCardPage.loadVirtualCardWithoutAmount();
+  });
+});
